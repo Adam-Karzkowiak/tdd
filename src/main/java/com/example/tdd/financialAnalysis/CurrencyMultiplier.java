49cdcfd9ab58c3295a5d8b35090d94780cca6400
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-06-14 21:24:08 +0200

    The first classes and methods that are based on tests have been created

---
+++ src/main/java/com/example/tdd/financialAnalysis/CurrencyMultiplier.java
@@ -0,0 +1,4 @@
+package com.example.tdd.financialAnalysis;
+
+class CurrencyMultiplier {
+}
---
+++ src/main/java/com/example/tdd/financialAnalysis/Dollar.java
@@ -0,0 +1,12 @@
+package com.example.tdd.financialAnalysis;
+
+class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+    }
+
+    void times(int multiplier) {
+
+    }
+}
